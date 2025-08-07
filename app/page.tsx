f8bd56fdc5de58e2fdd2d6fbe3a4fb5e22e28c2f
Aryal <roozanaryal8@gmail.com>
2025-08-07 18:13:12 +0545

    setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <>
       <div className="flex min-h-screen flex-col">This is Rojan Aryal</div>
+      <Button variant="outline">Hello Wordl</Button>
     </>
   );
 }
